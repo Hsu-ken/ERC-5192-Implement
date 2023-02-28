0ca3d7c5d63811f59eac0e9bb1c0ab5ec3810b52
Hsu-ken <qwe73320@gmail.com>
2023-02-28 23:59:22 +0800

    Delete useless

--- contracts/ERC5192SBT.sol
+++ contracts/ERC5192SBT.sol
@@ -4,22 +4,21 @@ import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./ERC5192.sol";
+
 contract ERC5192SBT is ERC5192 {
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
-  uint256 public totalSupply = 3333;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    uint256 public totalSupply = 3333;
 
-  constructor(string memory _name,string memory _symbol) ERC5192(_name,_symbol){
+    constructor(string memory _name, string memory _symbol)
+        ERC5192(_name, _symbol)
+    {}
 
-  }
- 
-   function mint(bool lock) public{
-    
-    _tokenIds.increment();
-    uint256 tokenId=_tokenIds.current();
-    require(tokenId <= totalSupply, "NFT Supply Full");
-    _mint(msg.sender, tokenId);
-    setTokenIdLock(tokenId,lock);
-    
-  }
+    function mint(bool lock) public {
+        _tokenIds.increment();
+        uint256 tokenId = _tokenIds.current();
+        require(tokenId <= totalSupply, "NFT Supply Full");
+        _mint(msg.sender, tokenId);
+        setTokenIdLock(tokenId, lock);
+    }
 }
